4f86e34cda69fcb28ad75a3663f16f9ee68de325
Ale <akip-core@outlook.com>
2021-10-12 10:17:18 +0200

    camera done! starting position

--- app/src/main/java/com/example/permessoex1g11/CameraActivity.kt
+++ app/src/main/java/com/example/permessoex1g11/CameraActivity.kt
@@ -1,11 +1,85 @@
 package com.example.permessoex1g11
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import java.util.jar.Manifest
 
 class CameraActivity : AppCompatActivity() {
+    val REQUEST_LOCATION_CODE = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        //inizio controllando che l'hardware camera sia rilevato
+        if(checkCameraHardware(this)){
+            val mPreview = CameraPreview(this, getCameraInstance())
+            // Set the Preview view as the content of our activity.
+            mPreview?.also {
+                val preview: FrameLayout = findViewById(R.id.camera_preview)
+                preview.addView(it)
+            }
+
+
+        }else{
+            Toast.makeText(this, "no camera detected", Toast.LENGTH_LONG).show()
+        }
     }
+
+
+
+    /** Check if this device has a camera */
+    private fun checkCameraHardware(context: Context): Boolean {
+        if (context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true
+        } else {
+            // no camera on this device
+            return false
+        }
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    fun getCameraInstance(): Camera? {
+        return try {
+            Camera.open() // attempt to get a Camera instance
+        } catch (e: Exception) {
+            // Camera is not available (in use or does not exist)
+            null // returns null if camera is unavailable
+        }
+    }
+
+    /** Richiesta permessi posizione */
+    private fun requestLocationPermission(){
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                getLastKnownLocation()
+            }
+            else -> requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_LOCATION_CODE -> {
+                if(grantResults.isNotEmpty() && grantResults.first() == PackageManager.PERMISSION_GRANTED){
+                    getLastKnownLocation()
+                }
+            }
+        }
+    }
+
 }
